89280b796884123b67e0e26e4787d687015793fc
Katie Bankhead <katie@bankheads.net>
2021-09-08 17:40:48 -0600

    Build initial card view

--- ConcentrationHW3/ContentView.swift
+++ ConcentrationHW3/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0 ..< 4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10).stroke()
+                Text("🧋")
+                    .font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 10)
+            }
+        }
+        .foregroundColor(.blue)
     }
 }
 
