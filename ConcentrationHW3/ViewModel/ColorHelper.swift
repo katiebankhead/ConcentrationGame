17b954a5b274af65fbc23aed29b7b30f3c2e2f05
Katie Bankhead <katie@bankheads.net>
2021-10-12 19:12:53 -0600

    Add ColorHelper conversion file

--- ConcentrationHW3/ViewModel/ColorHelper.swift
+++ ConcentrationHW3/ViewModel/ColorHelper.swift
@@ -5,4 +5,25 @@
 //  Created by Katie Bankhead on 10/12/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct ColorHelper {
+    static func color(for colorString: String) -> Color {
+        switch colorString {
+            case "orange":
+                return .orange
+            case "yellow":
+                return .yellow
+            case "green":
+                return .green
+            case "blue":
+                return .blue
+            case "purple":
+                return .purple
+            case "brown":
+                return .brown
+            default:
+                return .black
+        }
+    }
+}
